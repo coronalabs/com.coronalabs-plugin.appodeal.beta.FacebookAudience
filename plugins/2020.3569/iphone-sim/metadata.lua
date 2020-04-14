ae6544b5d2ebbf481a2e70f7fd3fd1c2d5d5377a
Vlad Shcherban <vlad@coronalabs.com>
2020-04-13 23:55:00 -0400

    Adding binaries

---
+++ plugins/2020.3569/iphone-sim/metadata.lua
@@ -0,0 +1,12 @@
+local metadata =
+{
+	plugin =
+	{
+		format = 'staticLibrary',
+		staticLibs = { 'APDFacebookAudienceAdapter', },
+		frameworks = { 'FBAudienceNetwork', 'FBSDKCoreKit', },
+		frameworksOptional = {},
+	},
+}
+
+return metadata
